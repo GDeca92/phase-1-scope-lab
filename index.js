07f0c3532453ee520889d4535e31fc43ee5c195d
Gian Carlo De Castro <Giancld92@gmail.com>
2022-09-01 08:27:05 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'value'
+
+upperCaseCustomerName = () => customerName = customerName.toUpperCase()
+
+setBestCustomer = () => bestCustomer = 'not bob'
+
+overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+
+changeLeastFavoriteCustomer = () => leastFavoriteCustomer = "karen"
